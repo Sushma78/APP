7e4d3301a830a29342262d1f884c44dd0625893c
Sushma78 <sushmasharma2536@gmail.com>
2024-02-25 12:59:21 +0530

    4files

---
+++ app.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading = React.createElement(
+  "h1",
+  {
+    id: "title",
+  },
+  "heading 1"
+);
+
+const headingY = React.createElement(
+  "h2",
+  {
+    id: "title",
+  },
+  "heading 2"
+);
+const container = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  [heading, headingY]
+);
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
